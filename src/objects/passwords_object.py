f698cd01412ee599455ee85f1c70f937bcb40395
uchanchlani <utkarsh.chanchlani@gmail.com>
2019-02-15 20:59:33 -0600

    more changes to the objects

--- src/objects/passwords_object.py
+++ src/objects/passwords_object.py
@@ -2,6 +2,7 @@ from helpers import date_helper
 import json
 import pickle
 
+
 class StorageNode:
     _MAX_LEVELS = 3
 
@@ -25,13 +26,12 @@ class StorageNode:
             self.children = {}
 
     def set_value(self, key, value):
+        self.last_modified = date_helper.curr_time_in_secs()
         if self.level == StorageNode._MAX_LEVELS:
             self.value = value
-            self.last_modified = date_helper.curr_time_in_secs()
             return
 
         if key == "":
-            self.last_modified = date_helper.curr_time_in_secs()
             return
 
         keys = key.split("/")
@@ -74,13 +74,30 @@ class StorageNode:
 
     def to_bytes(self):
         return pickle.dumps(self)
-        # return json.dumps(self, default=lambda o: o.__dict__,
-        #                   sort_keys=True, indent=4)
 
     @classmethod
     def from_bytes(cls, data):
         return pickle.loads(data)
 
+    def to_string(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
+
+    @classmethod
+    def from_string(cls, data):
+        def convert_util(obj, parent=None, key=None):
+            instance = StorageNode(parent, key)
+            for key in obj:
+                if key == "children":
+                    children = {}
+                    for child in obj[key]:
+                        children[child] = convert_util(obj[key][child], instance, child)
+                    setattr(instance, key, children);
+                else:
+                    setattr(instance, key, obj[key])
+
+            return instance
+
+        return convert_util(json.loads(data))
 
 # sNode = StorageNode()
 # sNode.set_value("google/gmail/utka", "asdf")
@@ -88,7 +105,8 @@ class StorageNode:
 # sNode.set_value("google/drive/utka", "asdf")
 # sNode.set_value("google/gmail/riya", "asdf")
 # print(sNode.get_values("google/gmail"))
-# print(sNode.to_bytes())
+# print(sNode.to_string())
 #
-# sNode2 = StorageNode.from_bytes(sNode.to_bytes())
-# print(sNode2.to_bytes())
+# sNode2 = StorageNode.from_string(sNode.to_string())
+# sNode2.remove_value("google/gmail/riya")
+# print(sNode2.to_string())
--- src/objects/passwords_patch.py
+++ src/objects/passwords_patch.py
@@ -1,3 +1,28 @@
-
 class PasswordPatch:
-    pass
+    def __init__(self, storage):
+        self.storage = storage
+        self.ops = {}
+
+    def set_value(self, key, value):
+        self.ops[key] = PasswordOperation(False, key, value)
+
+    def remove_value(self, key):
+        self.ops[key] = PasswordOperation(True, key)
+
+    def get_values(self, key):
+        if key in self.ops:
+            operation = self.ops[key]
+            if operation.del_op:
+                return None
+            else:
+                return [operation.value]
+        else:
+            return self.storage.get_values(key)
+
+
+class PasswordOperation:
+    def __init__(self, del_op, key, value=None):
+        self.del_op = del_op
+        self.key = key
+        if not self.del_op:
+            self.value = value
